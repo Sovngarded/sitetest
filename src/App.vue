f95cea170642a0f2836f1ee7133abd54c4ce76b8
Artem <kibirevaa@mer.ci.nsu.ru>
2021-11-23 20:52:55 +0700

    added table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <v-app style="background: #1C1D21;" >
+    <v-main >
+      <v-container >
+        <v-layout row wrap align-content-center>
+          <v-flex class="text-lg-center">
+
+
+        <div>
+        <h1 >
+        Kibirev Artem
+          </h1>
+        </div>
+        <div class="photo">
+
+          <img src="@/pictures/1.jpg">
+
+        </div>
+        <v-btn id="buttonOpenList"
+            color="pink"
+            dark
+        >ПОСМОТРЕТЬ РАСПИСАНИЕ</v-btn>
+
+
+
+
+
+
+
+      <v-data-table
+          :headers="headers"
+          :items="workthings"
+          :items-per-page="5"
+          class="elevation-1"
+      ></v-data-table>
+
+      </v-flex>
+      </v-layout>
+      </v-container>
+
+
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Что делаю',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Когда', value: 'when' },
+        { text: 'Зачем', value: 'why' },
+
+      ],
+      workthings: [
+        {
+          name: 'Встаю',
+          when:'10:00',
+          why:'а смысла нет каждый день'
+        },
+        {
+          name: 'Ем',
+          when:'10:20',
+          why:'вкусно поесть всегда надо'
+        },
+        {
+          name: 'Еду в колледж на любимой 43 маршрутке',
+          when:'10:40',
+          why:'как всегда опаздываю маршрутчику не прикажешь'
+        },
+        {
+          name: 'Впитываю информацию и жду обеда',
+          when:'10:45',
+          why:'ох когда же в столовку'
+        },
+        {
+          name: 'Ем',
+          when:'12:20',
+          why:'ох как вкусно'
+        },
+        {
+          name: 'Еду домой',
+          when:'14:35',
+          why:'Спасибо Клим Саныч. Потраченного времени жаль. Пятикратно переваренный кал.'
+        }
+      ],
+    }
+  },
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+img{
+  width: 500px;
+
+}
+
+
+
+h1{
+
+  font: italic small-caps bold 60px/2 cursive;
+  color:#BB9BB0;
+  text-shadow: 2px 2px 0px #ffffff, 5px 4px 0px rgba(0,0,0,0.15), 2px 2px 2px rgba(206,89,55,0);
 }
 </style>
